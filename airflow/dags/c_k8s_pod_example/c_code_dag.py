1e334b7ca31d720b135c8fb21da58a31ae4d199b
Wes Hahn <52631646+wesh92@users.noreply.github.com>
2025-09-19 09:10:36 -0400

    profiler deeper pt5

--- airflow/dags/c_k8s_pod_example/c_code_dag.py
+++ airflow/dags/c_k8s_pod_example/c_code_dag.py
@@ -21,38 +21,27 @@ def c_k8s_example():
     start = EmptyOperator(task_id="start")
     end = EmptyOperator(task_id="end")
 
-    @task
-    @profile
-    def run_cpu_intensive_pod(**kwargs):
-        """
-        Launches a KubernetesPodOperator to run the CPU-intensive prime number calculator.
-        The `@profile` decorator will capture the metrics of this Airflow task itself.
-        """
-        # This operator will create a pod that runs the C++ prime calculator.
-        kpo = KubernetesPodOperator(
-            task_id="prime_calculator_pod",
-            # Ensure this namespace is correct for your Airflow deployment
-            namespace="airflow",
-            image="cpp-math-app:latest",
-            image_pull_policy="Never",
-            name="cpu-intensive-pod",
-            do_xcom_push=False,
-            log_events_on_failure=True,
-            env_vars={
-                # Adjust this value to make the task run longer or shorter.
-                # 1,500,000 should take < 1 minute on modern hardware.
-                "MAX_PRIME": "1500000"
-            },
-            # Optional: Define resource requests and limits for the pod
-            container_resources=k8s.V1ResourceRequirements(
-                    requests={"cpu": "100m", "memory": "64Mi", "ephemeral-storage": "1Gi"},
-                    limits={"cpu": "200m", "memory": "420Mi", "ephemeral-storage": "2Gi"},
-                ),
-        )
-        # We must manually call execute() when wrapping an operator in a TaskFlow task
-        kpo.execute(context=kwargs)
+    kpo = KubernetesPodOperator(
+        task_id="prime_calculator_pod",
+        # Ensure this namespace is correct for your Airflow deployment
+        namespace="airflow",
+        image="cpp-math-app:latest",
+        image_pull_policy="Never",
+        name="cpu-intensive-pod",
+        do_xcom_push=False,
+        log_events_on_failure=True,
+        env_vars={
+            # Adjust this value to make the task run longer or shorter.
+            # 1,500,000 should take < 1 minute on modern hardware.
+            "MAX_PRIME": "1500000"
+        },
+        # Optional: Define resource requests and limits for the pod
+        container_resources=k8s.V1ResourceRequirements(
+                requests={"cpu": "100m", "memory": "64Mi", "ephemeral-storage": "1Gi"},
+                limits={"cpu": "200m", "memory": "420Mi", "ephemeral-storage": "2Gi"},
+            ),
+    )
 
-    run_pod = run_cpu_intensive_pod()
-    start >> run_pod >> end
+    start >> kpo >> end
 
 dag = c_k8s_example()
